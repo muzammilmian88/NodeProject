3b6012d43ff6b3e3a8327972d24d252b683f627a
muzammilmian88 <136157032+muzammilmian88@users.noreply.github.com>
2024-01-03 20:21:36 +0500

    Update Problem1.js

--- Problem1.js
+++ Problem1.js
@@ -1,6 +1,5 @@
 //Asynchronous Operations
 
-
 const axios = require('axios');
 
 const urls = ['https://jsonplaceholder.typicode.com/users', 'https://jsonplaceholder.typicode.com/posts', 'https://jsonplaceholder.typicode.com/todos'];
