c9a6f3de57b5564b2366b99dcdad6bd4a44194cd
muzammilmian88 <136157032+muzammilmian88@users.noreply.github.com>
2024-01-03 20:13:14 +0500

    Update Problem4.js

--- Problem4.js
+++ Problem4.js
@@ -1,11 +1,10 @@
 
 //Database Interaction
 
-
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const User = require('./model');
+const userRoutes = require('./routes/userRoutes');
 
 dotenv.config();
 
@@ -15,72 +14,11 @@ const PORT = process.env.PORT || 5000;
 console.log("******************");
 console.log(process.env);
 
-mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.Local_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(express.json());
 
-
-
-app.post('/users', async (req, res) => {
-  try {
-    const newUser = new User(req.body);
-    await newUser.save();
-
-
-    res.status(201).json(newUser);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-// Get all users
-app.get('/users', async (req, res) => {
-  try {
-    const users = await User.find();
-    res.status(200).json(users);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-
-app.get('/users/:id', async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id);
-    if (!user) {
-      return res.status(404).json({ error: 'User not found' });
-    }
-    res.status(200).json(user);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-
-app.put('/users/:id', async (req, res) => {
-  try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    if (!user) {
-      return res.status(404).json({ error: 'User not found' });
-    }
-    res.status(200).json(user);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-
-app.delete('/users/:id', async (req, res) => {
-  try {
-    const user = await User.findByIdAndDelete(req.params.id);
-    if (!user) {
-      return res.status(404).json({ error: 'User not found' });
-    }
-    res.status(204).send();
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+app.use('/users', userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
