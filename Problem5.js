3dc2ab52e0a61111bc0c56a7f543ee7585e0f325
muzammilmian88 <136157032+muzammilmian88@users.noreply.github.com>
2024-01-03 20:11:50 +0500

    Update Problem5.js

--- Problem5.js
+++ Problem5.js
@@ -1,60 +1,59 @@
-
 //Authentication
 
+require('dotenv').config();
 
 const express = require('express');
 const jwt = require('jsonwebtoken');
 
-const secretKey="secretkey";
+const secretKey = process.env.SECRET_KEY;
+const PORT = process.env.PORT || 3000;
+
 const app = express();
-app.get("/", (req,resp)=>{
+app.get("/", (req, resp) => {
     resp.json({
-        message:"Api is running"
+        message: "Api is running"
     })
 })
 
-app.post("/login", (req,resp)=>{
-    const user={
-        id:1,
-        name:"muzammil",
-        email:"muzammil@gmail.com"
+app.post("/login", (req, resp) => {
+    const user = {
+        id: 1,
+        name: "muzammil",
+        email: "muzammil@gmail.com"
     }
-    jwt.sign({user}, secretKey, {expiresIn:'300s'},(error, token)=>{
+    jwt.sign({ user }, secretKey, { expiresIn: '300s' }, (error, token) => {
         resp.json({
             token
         })
-
     })
 })
 
-app.post("/profile", verifyToken, (req,resp)=>{
-    jwt.verify(req.token, secretKey, (err,authData)=>{
-        if(err){
-            resp.send({result:"Invalid token"})
-        }else{
+app.post("/profile", verifyToken, (req, resp) => {
+    jwt.verify(req.token, secretKey, (err, authData) => {
+        if (err) {
+            resp.send({ result: "Invalid token" })
+        } else {
             resp.json({
-                message:"Profile accessed",
+                message: "Profile accessed",
                 authData
             })
         }
     })
-
 })
 
-function verifyToken(req,resp,next){
-    const bearerHeader=req.headers['authorization'];
+function verifyToken(req, resp, next) {
+    const bearerHeader = req.headers['authorization'];
 
-    if (typeof bearerHeader !== undefined){
-        const bearer=bearerHeader.split(" ");
-        const token=bearer[1];
-        req.token=token;
+    if (typeof bearerHeader !== undefined) {
+        const bearer = bearerHeader.split(" ");
+        const token = bearer[1];
+        req.token = token;
         next();
-
-    }else{
+    } else {
         resp.send("Token is Not valid");
     }
 }
 
-app.listen(5000, ()=>{
-    console.log("App is running on port 5000")
-})
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
